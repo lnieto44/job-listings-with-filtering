b8bac86b304738772fed45fd6da78145d7a7a2e7
Guillermmo Nieto <lnieto44@itfip.edu.co>
2022-03-20 05:42:49 -0500

    correcio

--- src/components/Cabecera/Cabecera.js
+++ src/components/Cabecera/Cabecera.js
@@ -2,15 +2,15 @@ import React, { Fragment } from "react";
 
 import "./Cabecera.css";
 
-export default function Cabecera({ filtro, EliminarEtiqueta, limpiar }) {
+export default function Cabecera({ filtered, EliminarEtiqueta, limpiar }) {
 	return (
 		<Fragment>
 			<div className="cabecera"></div>
-			{filtro.length > 0 && (
+			{filtered.length > 0 && (
 				<div className="Tablero">
 					<div className="lista-etiquetas">
-						{filtro
-							? filtro.map((etiqueta, index) => (
+						{filtered
+							? filtered.map((etiqueta, index) => (
 									<button key={index} onClick={e => EliminarEtiqueta(e.target.innerHTML)} className="etiqueta">
 										{etiqueta}
 									</button>
